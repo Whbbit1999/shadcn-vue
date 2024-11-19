f67ae44e930d3dac48d8773b825b223c6cd996ad
zernonia <zernonia@gmail.com>
2024-11-19 14:35:50 +0800

    chore: update new-york style too

--- apps/www/src/lib/registry/default/ui/sidebar/SidebarProvider.vue
+++ apps/www/src/lib/registry/default/ui/sidebar/SidebarProvider.vue
@@ -18,7 +18,7 @@ const emits = defineEmits<{
   'update:open': [open: boolean]
 }>()
 
-const isMobile = useMediaQuery('(max-width: 768px)') // useIsMobile()
+const isMobile = useMediaQuery('(max-width: 768px)')
 const openMobile = ref(false)
 
 const open = useVModel(props, 'open', emits, {
